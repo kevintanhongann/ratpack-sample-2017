d8f648ab1c522b0f66933f78b4eb11910faf54e2
kevintan <kevintanhongann@gmail.com>
2017-03-25 12:31:13 +0800

    abstracted CORSHandler into a separate class to be used

--- src/main/groovy/ratpack.groovy
+++ src/main/groovy/ratpack.groovy
@@ -2,21 +2,26 @@
 import static ratpack.groovy.Groovy.ratpack
 import ratpack.http.MutableHeaders
 
+import groovy.transform.CompileStatic
+import ratpack.groovy.handling.GroovyContext
+import ratpack.groovy.handling.GroovyHandler
 
-
+@CompileStatic
+class CORSHandler extends GroovyHandler {
+  @Override
+  protected void handle(GroovyContext context) {
+    MutableHeaders headers = context.response.headers
+    headers.set('Access-Control-Allow-Origin', '*')
+    headers.set('Access-Control-Allow-Headers', 'x-requested-with, origin, content-type, accept')
+    context.next()
+  }
+}
 
 ratpack {
   handlers {
 
     //Declare a new handler to handle all incoming requests regardless of method or path
-    all {
-      MutableHeaders headers = response.headers
-      //CORS headers
-      headers.set('Access-Control-Allow-Origin', '*')
-      headers.set('Access-Control-Allow-Headers', 'x-requested-with, origin, content-type, accept')
-      //Delegate processing to the next Handler in the chain
-      next()
-    }
+    all(new CORSHandler())
 
     get {
       render 'Hej Verden 2'
