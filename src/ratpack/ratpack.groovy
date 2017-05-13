503b41fd55f7888216ee089c382add91747b4df2
kevintan <kevintanhongann@gmail.com>
2017-05-13 10:46:53 +0800

    wip

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,15 +1,23 @@
 // @Grab('io.ratpack:ratpack-groovy:1.4.5')
 
+import ratpack.groovy.template.MarkupTemplateModule
+import ratpack.groovy.template.TextTemplateModule
 import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
+import asset.pipeline.ratpack.AssetPipelineModule
+import asset.pipeline.ratpack.AssetPipelineHandler
 
 ratpack {
+  bindings {
+    module(MarkupTemplateModule)
+    module(TextTemplateModule)
+  }
   handlers {
     //Declare a new handler to handle all incoming requests regardless of method or path
-    all(new CORSHandler())
+//    all(new CORSHandler())
 
     get {
-      render 'Hej Verden 2'
+      render groovyTemplate("index.html")
     }
 
     prefix('item') {
@@ -41,5 +49,10 @@ ratpack {
 
     }
 
+
+//    assets "static"
+    //Served by default from /assets or in this example from the root url
+    all(AssetPipelineHandler)
   }
+
 }
